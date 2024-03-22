faa3afae897ae77b3396d8de3b5c80d83aea6155
Angel Velasquez <pcisavel@upc.edu.pe>
2024-03-22 08:33:49 -0500

    feat: created relation between sales order and sales order item.

--- OOPSample/Sales/Domain/Model/Aggregates/SalesOrder.cs
+++ OOPSample/Sales/Domain/Model/Aggregates/SalesOrder.cs
@@ -5,5 +5,12 @@ public class SalesOrder(int id, int customerId)
     public int Id { get; } = id;
     public int CustomerId { get; } = customerId;
     
+    private readonly List<SalesOrderItem> _items = [];
+
+    public void AddItem(int productId, int quantity, double unitPrice)
+    {
+        _items.Add(new SalesOrderItem(Id, productId, quantity, unitPrice));
+    }
+    
     
 }
