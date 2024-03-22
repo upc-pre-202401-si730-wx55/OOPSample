e3ba714edafe6f8ad85eb44b20ca55ed0dc5853f
Angel Velasquez <pcisavel@upc.edu.pe>
2024-03-22 08:50:06 -0500

    feat: added shipping address to aggregate.

--- OOPSample/Sales/Domain/Model/Aggregates/SalesOrder.cs
+++ OOPSample/Sales/Domain/Model/Aggregates/SalesOrder.cs
@@ -1,3 +1,5 @@
+using OOPSample.Shared.Domain.Model.ValueObjects;
+
 namespace OOPSample.Sales.Domain.Model.Aggregates;
 
 public class SalesOrder(int id, int customerId)
@@ -7,10 +9,15 @@ public class SalesOrder(int id, int customerId)
     
     private readonly List<SalesOrderItem> _items = [];
 
+    public Address ShippingAddress { get; private set; }
     public void AddItem(int productId, int quantity, double unitPrice)
     {
         _items.Add(new SalesOrderItem(Id, productId, quantity, unitPrice));
     }
     
+    public void Dispatch(string street, string city, string state, string zipCode)
+    {
+        ShippingAddress = new Address(street, city, state, zipCode);
+    }
     
 }
---
+++ OOPSample/Shared/Domain/Model/ValueObjects/Address.cs
@@ -0,0 +1,3 @@
+namespace OOPSample.Shared.Domain.Model.ValueObjects;
+
+public record Address(string Street, string City, string State, string ZipCode);
