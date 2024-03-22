e3dfa194d930c597ab483b7896f0488e17f945ae
Angel Velasquez <pcisavel@upc.edu.pe>
2024-03-22 08:25:26 -0500

    feat: added sales order item.

---
+++ OOPSample/Sales/Domain/Model/Aggregates/SalesOrderItem.cs
@@ -0,0 +1,16 @@
+namespace OOPSample.Sales.Domain.Model.Aggregates;
+
+public class SalesOrderItem(int salesOrderId, int productId, int quantity, double unitPrice)
+{
+    public Guid Id { get;  } = GenerateOrderItemId();
+    public int SalesOrderId { get; } = salesOrderId;
+    public int ProductId { get; } = productId;
+    public int Quantity { get; } = quantity;
+    public double UnitPrice { get; } = unitPrice;
+
+    private static Guid GenerateOrderItemId()
+    {
+        return Guid.NewGuid();
+    }
+    
+}
