752d9e0b3c22ccdfae73866b4611ddae89407223
Angel Velasquez <pcisavel@upc.edu.pe>
2024-03-23 08:47:31 -0500

    feat: added business logic to sales order aggregate.

--- OOPSample/Sales/Domain/Model/Aggregates/SalesOrder.cs
+++ OOPSample/Sales/Domain/Model/Aggregates/SalesOrder.cs
@@ -12,13 +12,24 @@ public class SalesOrder(int id, int customerId)
     public SalesOrderStatus Status { get; private set; } = SalesOrderStatus.PendingPayment;
 
     public Address ShippingAddress { get; private set; }
+
+    public double PaidAmount { get; private set; } = 0;
+    
     public void AddItem(int productId, int quantity, double unitPrice)
     {
+        if (Status != SalesOrderStatus.PendingPayment)
+            throw new InvalidOperationException("Can't modify an order once payment is processed.");
         _items.Add(new SalesOrderItem(Id, productId, quantity, unitPrice));
     }
     
     public void Dispatch(string street, string city, string state, string zipCode)
     {
+        if (Status == SalesOrderStatus.PendingPayment)
+            throw new InvalidOperationException("Can't dispatch an order that is not paid yet.");
+        
+        if (_items.Count == 0)
+            throw new InvalidOperationException("Can't dispatch an order without items.");
+        
         ShippingAddress = new Address(street, city, state, zipCode);
         Status = SalesOrderStatus.Shipped;
     }
@@ -33,4 +44,24 @@ public class SalesOrder(int id, int customerId)
         Status = SalesOrderStatus.Completed;
     }
     public double CalculateTotalPrice() => _items.Sum(x => x.CalculateItemPrice());
+
+    public void AddPayment(double amount)
+    {
+        if (amount <= 0)
+            throw new InvalidOperationException("Amount must be greater than zero.");
+        
+        if (amount > CalculateTotalPrice() - PaidAmount)
+            throw new InvalidOperationException("Amount must be less than or equal to the total price.");
+        PaidAmount += amount;
+        VerifyIfReadyForShipment();
+    }
+    
+    private void VerifyIfReadyForShipment()
+    {
+        if (PaidAmount == CalculateTotalPrice())
+        {
+            Status = SalesOrderStatus.ReadyForShipment;
+        }
+        
+    }
 }
